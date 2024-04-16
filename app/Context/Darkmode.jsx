925e8cb372e3f21362fd97a1fa50170614b94976
shahisrail <shahisrail134@gmail.com>
2024-04-16 20:28:08 +0600

    root not found page and dynamic route not found page and dark light set in local Storage

--- app/Context/Darkmode.jsx
+++ app/Context/Darkmode.jsx
@@ -1,10 +1,19 @@
 "use client";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const ThemeContext = createContext();
 
 const Darkmode = ({ children }) => {
-  const [darkMode, setDarkMode] = useState(false);
+  // Retrieve the initial darkMode state from local storage or default to false
+  const [darkMode, setDarkMode] = useState(() => {
+    const storedDarkMode = localStorage.getItem("darkMode");
+    return storedDarkMode ? JSON.parse(storedDarkMode) : false;
+  });
+
+  // Update local storage whenever darkMode state changes
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+  }, [darkMode]);
 
   return (
     <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
--- app/not-found.js
+++ app/not-found.js
@@ -1,15 +1,11 @@
-"use client";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 export default function NotFound() {
-  const pathName = usePathname();
-
   return (
     <div>
       <h2 className="text-center text-3xl ">Not Found 🙁😢</h2>
       <p className="text-center text-3xl ">
-        Could not find requested this movie {pathName} resource from length{" "}
+        Could not find requested this movie resource from length{" "}
       </p>
       <div className="flex justify-center">
         <Link href="/">
